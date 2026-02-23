c89b4251a15af30cbe1a9aaf59af192134758684
nimishaaaaaw <nimishadadajimajgawali2022@vitbhopal.ac.in>
2026-02-23 19:24:36 +0530

    Update main.py with HF Space fixes

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -78,4 +78,4 @@ def update_item(prediction_input: PredictionIn):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
